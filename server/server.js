e1bd749a4dc36088944ca9d68668c793bc5cb403
Artjom <giga.tjom@gmail.com>
2021-01-15 18:02:03 +0200

    mongo db added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const io = require('socket.io')(5000)
+const mongo = require('mongodb').MongoClient
 
-io.on('connection', socket => {
-    const id = socket.handshake.query.id
-    socket.join(id)
-    
-    socket.on('send-message', ({recipients, text}) => {
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r => r !== recipient)
-            newRecipients.push(id)
-            socket.broadcast.to(recipient).emit('receive-message', {
-                recipients: newRecipients, sender: id, text
+const mongourl = 'mongodb://127.0.0.1/whatsup-clone'
+
+mongo.connect(mongourl, { useUnifiedTopology: true }, (err, db) => {
+    if (err) throw err;
+
+    console.log("db connected....")
+
+    io.on('connection', socket => {
+        const id = socket.handshake.query.id
+        socket.join(id)
+        socket.on('send-message', ({recipients, text}) => {
+            recipients.forEach(recipient => {
+                const newRecipients = recipients.filter(r => r !== recipient)
+                newRecipients.push(id)
+                socket.broadcast.to(recipient).emit('receive-message', {
+                    recipients: newRecipients, sender: id, text
+                })
             })
-        });
+        })
     })
 })
