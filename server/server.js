f912c8b2689cd3c99b75e6c03df5780e244bc4cd
Artjom <giga.tjom@gmail.com>
2021-01-26 21:26:23 +0200

    db

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const io = require('socket.io')(5000)
 const mongo = require('mongodb').MongoClient
 
-const mongourl = 'mongodb://127.0.0.1/whatsup-clone'
+const mongourl = 'mongodb://127.0.0.1'
 
-mongo.connect(mongourl, { useUnifiedTopology: true }, (err, db) => {
+mongo.connect(mongourl, { useUnifiedTopology: true }, (err, client) => {
     if (err) throw err;
+    const db = client.db('messenger')
 
     console.log("db connected....")
 
     io.on('connection', socket => {
         const id = socket.handshake.query.id
         socket.join(id)
-        socket.on('send-message', ({recipients, text}) => {
+        db.collection("users").updateOne({ userId: id }, { $set: { userId: id }, $inc: { connection: 1 } }, { upsert: true })
+
+        socket.on('send-message', ({ recipients, text }) => {
             recipients.forEach(recipient => {
                 const newRecipients = recipients.filter(r => r !== recipient)
                 newRecipients.push(id)
